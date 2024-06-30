89fa1c31d5104ef597d2c483e34228eabd114b39
VGcod3 <vktrgavryliuk@gmail.com>
2024-07-01 01:47:01 +0300

    refactred code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Button, Container, TextField, Box } from '@mui/material';
-import { Line } from 'react-chartjs-2';
+import { useEffect } from "react";
+import { Button, Container, TextField, Box, Grid } from "@mui/material";
+import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,7 +10,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
+import { dataSchema, useESP } from "./useESP";
 
 ChartJS.register(
   CategoryScale,
@@ -22,45 +23,48 @@ ChartJS.register(
   Legend
 );
 
-const App: React.FC = () => {
-  const [speed, setSpeed] = useState('');
-  const [duration, setDuration] = useState('');
-  const [startTime, setStartTime] = useState<number | null>(null);
-  const [data, setData] = useState({
-    labels: [] as string[],
-    datasets: [
-      {
-        label: 'Thrust',
-        data: [] as number[],
-        borderColor: 'rgba(255, 99, 132, 1)',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      },
-      {
-        label: 'Torque',
-        data: [] as number[],
-        borderColor: 'rgba(54, 162, 235, 1)',
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      },
-      {
-        label: 'Throttle',
-        data: [] as number[],
-        borderColor: 'rgba(255, 206, 86, 1)',
-        backgroundColor: 'rgba(255, 206, 86, 0.2)',
-      },
-      {
-        label: 'Voltage',
-        data: [] as number[],
-        borderColor: 'rgba(75, 192, 192, 1)',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-      },
-    ],
-  });
-  const [isTestButtonPressed, setIsTestButtonPressed] = useState(false);
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const dataQueue = useRef<any[]>([]);
+const addZero = (num: number) => (num < 10 ? `0${num}` : num);
 
+const timePresets = [
+  {
+    label: "+5s",
+    value: 5,
+  },
+  {
+    label: "+15s",
+    value: 15,
+  },
+  {
+    label: "+30s",
+    value: 30,
+  },
+  {
+    label: "+1m",
+    value: 60,
+  },
+];
+
+const App = () => {
+  const {
+    duration,
+    socket,
+    setSocket,
+    dataQueue,
+    speed,
+    setData,
+    startTime,
+    setSpeed,
+    setDuration,
+    handleStartReadings,
+    isTestRunning,
+    handleStopReadings,
+    handleClearGraph,
+    data,
+  } = useESP();
+
+  // Connect to the WebSocket server
   useEffect(() => {
-    const ws = new WebSocket('ws://192.168.0.150:8080');
+    const ws = new WebSocket("ws://172.20.10.150:8080");
     setSocket(ws);
 
     return () => {
@@ -70,143 +74,166 @@ const App: React.FC = () => {
     };
   }, []);
 
+  // Listen for incoming messages from the WebSocket server
   useEffect(() => {
     if (!socket) return;
 
     socket.onmessage = (event) => {
       const newData = JSON.parse(event.data);
-      dataQueue.current.push(newData);
+
+      const parsed = dataSchema.safeParse(newData);
+
+      if (!parsed.success) {
+        return alert("Fetched data in wrong format");
+      }
+
+      dataQueue.current.push(parsed.data);
     };
   }, [socket]);
 
+  // Refresh the graph data every 100ms
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (dataQueue.current.length > 0) {
-        const batchData = dataQueue.current;
-        dataQueue.current = [];
-
-        const averages = batchData.reduce((acc, curr) => {
-          Object.keys(curr).forEach((key) => {
-            if (!acc[key]) {
-              acc[key] = [];
-            }
-            acc[key].push(curr[key]);
-          });
-          return acc;
-        }, {} as Record<string, number[]>);
-
-        Object.keys(averages).forEach((key) => {
-          averages[key] = averages[key].reduce((a: number, b: number) => a + b, 0) / averages[key].length;
-        });
-
-        setData((prevData) => {
-          const currentTime = new Date();
-          const elapsedTime = Math.floor((currentTime.getTime() - (startTime || currentTime.getTime())) / 1000);
-          const minutes = String(Math.floor(elapsedTime / 60)).padStart(2, '0');
-          const seconds = String(elapsedTime % 60).padStart(2, '0');
-          const formattedTime = `${minutes}:${seconds}`;
-
-          return {
-            ...prevData,
-            labels: [...prevData.labels, formattedTime],
-            datasets: prevData.datasets.map((dataset, index) => ({
-              ...dataset,
-              data: [...dataset.data, averages[Object.keys(averages)[index]]],
-            })),
-          };
-        });
-      }
-    }, 100);
+    const intervalId = setInterval(refreshGraphData, 100);
 
     return () => clearInterval(intervalId);
   }, [startTime]);
 
-  const handleStartReadings = () => {
-    if (!socket || !speed || !duration) return;
+  const refreshGraphData = () => {
+    if (dataQueue.current.length <= 0) return;
 
-    const message = JSON.stringify({ type: 'start', duration, speed });
-    socket.send(message);
-    setIsTestButtonPressed(true);
-    setStartTime(new Date().getTime());
+    const batchData = dataQueue.current;
+    dataQueue.current = [];
 
-    // Reset state after the duration has passed
-    setTimeout(() => {
-      setSpeed('');
-      setDuration('');
-      setIsTestButtonPressed(false);
-      setStartTime(null);
-    }, parseInt(duration) * 1000); // Convert duration to milliseconds
-  };
+    const averages = batchData.reduce((acc, curr) => {
+      Object.keys(curr).forEach((key) => {
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push(curr[key]);
+      });
+      return acc;
+    });
 
-  const handleStopReadings = () => {
-    if (!socket) return;
+    console.log(averages);
 
-    const message = JSON.stringify({ type: 'stop' });
-    socket.send(message);
-    setIsTestButtonPressed(false);
-    setStartTime(null);
-  };
+    Object.keys(averages).forEach((key) => {
+      averages[key] =
+        averages[key].reduce((a: number, b: number) => a + b, 0) /
+        averages[key].length;
+    });
 
-  const handleClearGraph = () => {
-    setData({
-      labels: [],
-      datasets: data.datasets.map((dataset) => ({
-        ...dataset,
-        data: [],
-      })),
+    const currentTime = new Date().getTime();
+
+    const timeElapsedInSeconds = Math.floor(
+      (currentTime - (startTime || currentTime)) / 1000
+    );
+
+    const minutes = addZero(Math.floor(timeElapsedInSeconds / 60));
+    const seconds = addZero(timeElapsedInSeconds % 60);
+
+    setData((prevData) => {
+      return {
+        ...prevData,
+        labels: [...prevData.labels, `${minutes}:${seconds}`],
+        datasets: prevData.datasets.map((dataset, index) => ({
+          ...dataset,
+          data: [...dataset.data, averages[Object.keys(averages)[index]]],
+        })),
+      };
     });
   };
 
   return (
     <Container>
       <Box my={4}>
-        <TextField
-          label="Speed"
-          variant="outlined"
-          value={speed}
-          onChange={(e) => setSpeed(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Duration"
-          variant="outlined"
-          value={duration}
-          onChange={(e) => setDuration(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleStartReadings}
-          disabled={!speed || !duration}
-          fullWidth
-          sx={{ mt: 2, mr: 1, bgcolor: isTestButtonPressed ? 'success.dark' : 'primary.main' }}
-        >
-          {isTestButtonPressed ? 'Test (Active)' : 'Test'}
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={handleStopReadings}
-          fullWidth
-          sx={{ mt: 2, mr: 1 }}
-        >
-          Stop
-        </Button>
-        <Button
-          variant="contained"
-          color="warning"
-          onClick={handleClearGraph}
-          fullWidth
-          sx={{ mt: 2 }}
-        >
-          Clear Graph
-        </Button>
+        <Line data={data} />
       </Box>
       <Box my={4}>
-        <Line data={data} />
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={4}>
+            <TextField
+              label="Speed"
+              variant="outlined"
+              type="number"
+              value={speed}
+              onChange={(e) => setSpeed(+e.target.value)}
+              fullWidth
+              margin="normal"
+              disabled={isTestRunning}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              label="Duration (seconds)"
+              variant="outlined"
+              type="number"
+              value={duration}
+              onChange={(e) =>
+                setDuration(+e.target.value >= 0 ? +e.target.value : 0)
+              }
+              fullWidth
+              margin="normal"
+              disabled={isTestRunning}
+            />
+          </Grid>
+          <Grid item xs={4} spacing={1} container sx={{ pb: 3 }}>
+            {timePresets.map((btn, index) => (
+              <Button
+                variant="text"
+                disabled={isTestRunning}
+                key={index}
+                onClick={() => setDuration((prev) => (prev += btn.value))}
+                sx={{
+                  mt: 2,
+                  height: "100%",
+                }}
+              >
+                {btn.label}
+              </Button>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} columns={3}>
+          <Grid item xs={1}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleStartReadings}
+              disabled={!speed || !duration || isTestRunning}
+              fullWidth
+              sx={{
+                mt: 2,
+                mr: 1,
+                bgcolor: isTestRunning ? "success.dark" : "primary.main",
+              }}
+            >
+              {isTestRunning ? "Test (Active)" : "Test"}
+            </Button>
+          </Grid>
+          <Grid item xs={1}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleStopReadings}
+              fullWidth
+              sx={{ mt: 2, mr: 1 }}
+            >
+              Stop
+            </Button>
+          </Grid>
+          <Grid item xs={1}>
+            <Button
+              variant="contained"
+              color="warning"
+              onClick={handleClearGraph}
+              fullWidth
+              sx={{ mt: 2 }}
+            >
+              Clear Graph
+            </Button>
+            '
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
---
+++ src/useESP.ts
@@ -0,0 +1,112 @@
+import { useState, useRef } from "react";
+import { z } from "zod";
+
+export const dataSchema = z.object({
+  thrust: z.number(),
+  torque: z.number(),
+  voltage: z.number(),
+  current: z.number(),
+});
+
+type DateSchema = z.infer<typeof dataSchema>;
+
+const initData = {
+  labels: [] as string[],
+  datasets: [
+    {
+      label: "Thrust",
+      data: [] as DateSchema[],
+      borderColor: "rgba(255, 99, 132, 1)",
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+    },
+    {
+      label: "Torque",
+      data: [] as DateSchema[],
+      borderColor: "rgba(54, 162, 235, 1)",
+      backgroundColor: "rgba(54, 162, 235, 0.2)",
+    },
+    {
+      label: "Voltage",
+      data: [] as DateSchema[],
+      borderColor: "rgba(75, 192, 192, 1)",
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+    },
+    {
+      label: "Current",
+      data: [] as DateSchema[],
+      borderColor: "rgb(58, 201, 68)",
+      backgroundColor: "rgba(77, 192, 75, 0.2)",
+    },
+  ],
+};
+
+export const useESP = () => {
+  const [speed, setSpeed] = useState(800);
+  const [duration, setDuration] = useState(0);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [espStateData, updateESPData] = useState(initData);
+  const [isTestRunning, setIsTestRunning] = useState(false);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  const dataQueue = useRef<any[]>([]);
+
+  const handleStartReadings = async () => {
+    if (!socket || !speed || !duration) return;
+
+    const message = JSON.stringify({
+      type: "start",
+      duration: duration * 1000,
+      speed,
+    });
+    socket.send(message);
+    setIsTestRunning(true);
+    setStartTime(new Date().getTime());
+
+    // Reset state after the duration has passed
+    setTimeout(() => {
+      setSpeed(() => 800);
+      setDuration(() => 0);
+      setIsTestRunning(() => false);
+      setStartTime(null);
+    }, duration * 1000); // Convert duration to milliseconds
+  };
+
+  const handleStopReadings = () => {
+    if (!socket) return;
+
+    const message = JSON.stringify({ type: "stop" });
+    socket.send(message);
+    setIsTestRunning(false);
+    setStartTime(null);
+  };
+
+  const handleClearGraph = () => {
+    updateESPData({
+      labels: [],
+      datasets: espStateData.datasets.map((dataset) => ({
+        ...dataset,
+        data: [],
+      })),
+    });
+  };
+
+  return {
+    speed,
+    setSpeed,
+    duration,
+    setDuration,
+    startTime,
+    setStartTime,
+    data: espStateData,
+    setData: updateESPData,
+    isTestRunning,
+    setIsTestRunning,
+    socket,
+    setSocket,
+    dataQueue,
+    handleStartReadings,
+
+    handleStopReadings,
+    handleClearGraph,
+  };
+};
