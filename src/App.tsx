4e9a044030f57a73ee98a0ee8ef832fc849867d3
MoonLighTingPY <romanfigelo@gmail.com>
2024-07-23 01:05:35 +0300

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './index.css'
+import "./index.css";
 import { useEffect, useRef, useState } from "react";
 import { Button, TextField, Box, Grid, Typography } from "@mui/material";
 import { Line } from "react-chartjs-2";
@@ -15,7 +15,6 @@ import {
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 import { dataSchema, useESP } from "./useESP";
-
 
 ChartJS.register(
   CategoryScale,
@@ -37,13 +36,11 @@ const timePresets = [
 const speedPresets = [
   { label: "+", value: 50 },
   { label: "-", value: -50 },
-
 ];
 
 const App = () => {
   const chartRef = useRef(null);
   const [stats, setStats] = useState({
-    
     thrustMax: 0,
     thrustMin: 0,
     torqueMax: 0,
@@ -72,7 +69,6 @@ const App = () => {
     handleClearGraph,
     data,
   } = useESP();
-
 
   // Connect to the WebSocket server
   useEffect(() => {
@@ -122,7 +118,6 @@ const App = () => {
     return () => clearInterval(intervalId);
   }, [startTime]);
 
-  
   const refreshGraphData = () => {
     if (dataQueue.current.length <= 0) return;
 
@@ -141,7 +136,8 @@ const App = () => {
 
     Object.keys(averages).forEach((key) => {
       averages[key] =
-        averages[key].reduce((a: any, b: any) => a + b, 0) / averages[key].length;
+        averages[key].reduce((a: any, b: any) => a + b, 0) /
+        averages[key].length;
     });
 
     const currentTime = new Date().getTime();
@@ -163,8 +159,6 @@ const App = () => {
     }));
   };
 
-  
-
   const handleSaveAsPDF = async () => {
     const chartElement = chartRef.current;
     if (!chartElement) return;
@@ -180,8 +174,7 @@ const App = () => {
     pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
 
     // Add stats to PDF
-    const statsText = 
-    `
+    const statsText = `
     Stats:
 
     THRUST Max: ${stats.thrustMax} Min: ${stats.thrustMin}
@@ -190,8 +183,6 @@ const App = () => {
     CURRENT Max: ${stats.currentMax} Min: ${stats.currentMin}
 
     `;
-    
-    
 
     pdf.text(statsText, 120, pdfHeight + 10);
 
@@ -199,7 +190,9 @@ const App = () => {
     const inputsText = `
     Test info:
 
-    Duration: ${Math.floor(duration / 60)} minutes ${addZero(duration % 60)} seconds
+    Duration: ${Math.floor(duration / 60)} minutes ${addZero(
+      duration % 60
+    )} seconds
     Speed: ${speed}
     Motor Model: ${motorModel}
     Propeller Model: ${propellerModel}`;
@@ -209,210 +202,294 @@ const App = () => {
     pdf.save("chart.pdf");
   };
 
-  
   const handleConfigWifi = () => {
     window.location.href = "http://esp32-motortester.local/config";
   };
 
-  const timeRemaining = Math.max(0, duration - Math.floor((Date.now() - startTime!) / 1000));
+  const timeRemaining = Math.max(
+    0,
+    duration - Math.floor((Date.now() - startTime!) / 1000)
+  );
 
   return (
     <div className="App">
-    <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-      <Box sx={{ display: "flex", flexDirection: "row", flexGrow: 1 }}>
-        
-        {/* Chart */}
-        <Box sx={{ flex: 3 }}>
-        <div className="chart-container" ref={chartRef}>
-        <div className="section-box">
+      <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+        <Box sx={{ display: "flex", flexDirection: "row", flexGrow: 1 }}>
+          {/* Chart */}
+          <Box sx={{ flex: 3 }}>
+            <div className="chart-container" ref={chartRef}>
+              <div className="section-box">
+                <Line
+                  data={data}
+                  options={{
+                    animation: false,
+                    responsive: true,
+                    maintainAspectRatio: false,
+                  }}
+                />
+              </div>
+            </div>
+          </Box>
 
-    <Line 
-      data={data} 
-      options={{ 
-        animation: false, 
-        responsive: true, 
-        maintainAspectRatio: false 
-      }} 
-    />
-  </div>
+          {/* Right side content: Statistics, buttons, and controls */}
+          <Box
+            sx={{ flex: 1, ml: 2, display: "flex", flexDirection: "column" }}
+          >
+            {/* Statistics */}
+            <div className="section-box">
+              <Box sx={{ mb: 2, overflowY: "auto" }}>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold", fontSize: "2em" }}>
+                    THRUST
+                  </span>{" "}
+                  Max:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.thrustMax}</span>{" "}
+                  Min:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.thrustMin}</span>
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold", fontSize: "2em" }}>
+                    TORQUE
+                  </span>{" "}
+                  Max:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.torqueMax}</span>{" "}
+                  Min:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.torqueMin}</span>
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold", fontSize: "2em" }}>
+                    VOLTAGE
+                  </span>{" "}
+                  Max:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.voltageMax}</span>{" "}
+                  Min:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.voltageMin}</span>
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold", fontSize: "2em" }}>
+                    CURRENT
+                  </span>{" "}
+                  Max:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.currentMax}</span>{" "}
+                  Min:{" "}
+                  <span style={{ fontWeight: "bold" }}>{stats.currentMin}</span>
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold" }}>Speed:</span>{" "}
+                  <span style={{ fontWeight: "bold" }}>{speed}</span>
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold" }}> Duration:</span>{" "}
+                  <span style={{ fontWeight: "bold" }}>{duration} seconds</span>
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold" }}> Motor Model:</span>{" "}
+                  {motorModel}
+                </Typography>
+                <Typography variant="body1">
+                  <span style={{ fontWeight: "bold" }}> Propeller Model:</span>{" "}
+                  {propellerModel}
+                </Typography>
+              </Box>
+            </div>
+            {/* Buttons */}
+            <div className="section-box">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 3,
+                }}
+              >
+                <img
+                  src="/images/play.ico"
+                  alt="Test"
+                  onClick={handleStartReadings}
+                  style={{
+                    cursor: "pointer",
+                    width: "15%",
+                    height: "auto",
+                    marginLeft: "5",
+                  }}
+                />
 
-  </div>
-        
-      
-  
+                <img
+                  src="/images/stop.ico"
+                  alt="Stop"
+                  onClick={handleStopReadings}
+                  style={{ cursor: "pointer", width: "15%", height: "auto" }}
+                />
+                <img
+                  src="/images/clear.ico"
+                  alt="Clear Graph"
+                  onClick={handleClearGraph}
+                  style={{ cursor: "pointer", width: "15%", height: "auto" }}
+                />
+                <img
+                  src="/images/pdf.ico"
+                  alt="Save as PDF"
+                  onClick={handleSaveAsPDF}
+                  style={{ cursor: "pointer", width: "16%", height: "auto" }}
+                />
+                <img
+                  src="/images/gear.ico"
+                  alt="Configure WiFi"
+                  onClick={handleConfigWifi}
+                  style={{ cursor: "pointer", width: "15%", height: "auto" }}
+                />
+              </Box>
+              <Typography
+                variant="body1"
+                sx={{
+                  ml: 2,
+                  fontWeight: "bold",
+                  fontSize: "1.2em",
+                  color: "#333",
+                  textAlign: "center",
+                }}
+              >
+                {`Time remaining: ${Math.floor(timeRemaining / 60)}:${addZero(
+                  timeRemaining % 60
+                )}`}
+              </Typography>
+            </div>
+          </Box>
         </Box>
-        
-        {/* Right side content: Statistics, buttons, and controls */}
-        <Box sx={{flex: 1, ml: 2, display: "flex", flexDirection: "column" }}>
-          {/* Statistics */}
-          <div className="section-box">
-          <Box sx={{ mb: 2, overflowY: "auto" }}>
-    <Typography variant="body1">
-      <span style={{ fontWeight: "bold", fontSize: "2em" }}>THRUST</span> Max: <span style={{ fontWeight: "bold" }}>{stats.thrustMax}</span> Min: <span style={{ fontWeight: "bold" }}>{stats.thrustMin}</span>
-    </Typography>
-    <Typography variant="body1">
-      <span style={{ fontWeight: "bold", fontSize: "2em" }}>TORQUE</span> Max: <span style={{ fontWeight: "bold" }}>{stats.torqueMax}</span> Min: <span style={{ fontWeight: "bold" }}>{stats.torqueMin}</span>
-    </Typography>
-    <Typography variant="body1">
-      <span style={{ fontWeight: "bold", fontSize: "2em" }}>VOLTAGE</span> Max: <span style={{ fontWeight: "bold" }}>{stats.voltageMax}</span> Min: <span style={{ fontWeight: "bold" }}>{stats.voltageMin}</span>
-    </Typography>
-    <Typography variant="body1">
-      <span style={{ fontWeight: "bold", fontSize: "2em" }}>CURRENT</span> Max: <span style={{ fontWeight: "bold" }}>{stats.currentMax}</span> Min: <span style={{ fontWeight: "bold" }}>{stats.currentMin}</span>
-    </Typography>
-    <Typography variant="body1" >
-      <span style={{ fontWeight: "bold" }}>Speed:</span> <span style={{ fontWeight: "bold" }}>{speed}</span>
-      </Typography>
-      <Typography variant="body1" >
-      <span style={{ fontWeight: "bold" }}> Duration:</span>{" "}<span style={{ fontWeight: "bold" }}>{duration} seconds</span>
-      </Typography>
-      <Typography variant="body1" >
-      <span style={{ fontWeight: "bold" }}> Motor Model:</span> {motorModel}
-      </Typography>
-      <Typography variant="body1" >
-      <span style={{ fontWeight: "bold" }}> Propeller Model:</span> {propellerModel}
-    </Typography>
-  </Box>
-  </div>
-          {/* Buttons */}
-<div className="section-box">
-  <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 3}}>
-    <img
-      src="/images/play.ico"
-      alt="Test"
-      onClick={handleStartReadings}
-      style={{ cursor: 'pointer', width: '15%', height: 'auto', marginLeft: "5"}}
-    />
 
-    <img
-      src="/images/stop.ico"
-      alt="Stop"
-      onClick={handleStopReadings}
-      style={{ cursor: 'pointer', width: '15%', height: 'auto' }}
-    />
-    <img
-      src="/images/clear.ico"
-      alt="Clear Graph"
-      onClick={handleClearGraph}
-      style={{ cursor: 'pointer', width: '15%', height: 'auto' }}
-    />
-    <img
-      src="/images/pdf.ico"
-      alt="Save as PDF"
-      onClick={handleSaveAsPDF}
-      style={{ cursor: 'pointer', width: '16%', height: 'auto' }}
-    />
-    <img
-      src="/images/gear.ico"
-      alt="Configure WiFi"
-      onClick={handleConfigWifi}
-      style={{ cursor: 'pointer', width: '15%', height: 'auto' }}
-    />
-  </Box>
-  <Typography variant="body1" sx={{ ml: 2, fontWeight: 'bold', fontSize: '1.2em', color: '#333', textAlign: 'center' }}>
-    {`Time remaining: ${Math.floor(timeRemaining / 60)}:${addZero(timeRemaining % 60)}`}
-  </Typography>
-</div>
-        </Box>       
-      </Box>
-      
-      <div className="section-box">
-
-      <Grid container spacing={2} alignItems="center" sx={{ marginBottom: 2, ml:4 }}>
-        
-      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", mt: 4.75}}>
-          {/* Motor Model Input */}
-          <Grid item xs={12} sx={{ mr: 1 }}>
-            <TextField
-              label="Motor Model"
-              variant="outlined"
-              value={motorModel}
-              onChange={(e) => setMotorModel(e.target.value)}
-              fullWidth
-              size="small"
-              margin="none"
-              disabled={isTestRunning}
-            />
-          </Grid>
-          {/* Propeller Model Input */}
-          <Grid item xs={12}>
-            <TextField
-              label="Propeller Model"
-              variant="outlined"
-              value={propellerModel}
-              onChange={(e) => setPropellerModel(e.target.value)}
-              fullWidth
-              size="small"
-              margin="none"
-              disabled={isTestRunning}
-            />
-          </Grid>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", mt: 4.75, ml: 5 }}>
-          {/* Speed Input and Presets */}
-            <TextField
-              label="Speed"
-              variant="outlined"
-              value={speed}
-              type="number"
-              onChange={(e) => setSpeed(Number(e.target.value))}
-              fullWidth
-              size="small"
-              margin="none"
-              disabled={isTestRunning}
-              style={{ width: '18ex', minWidth: 'auto' }}
-            />
-          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>  
-            {speedPresets.map((btn, index) => (
-              <Button
-                key={index}
+        <div className="section-box">
+          <Grid
+            container
+            spacing={2}
+            alignItems="center"
+            sx={{ marginBottom: 2, ml: 4 }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                mt: 4.75,
+              }}
+            >
+              {/* Motor Model Input */}
+              <Grid item xs={12} sx={{ mr: 1 }}>
+                <TextField
+                  label="Motor Model"
+                  variant="outlined"
+                  value={motorModel}
+                  onChange={(e) => setMotorModel(e.target.value)}
+                  fullWidth
+                  size="small"
+                  margin="none"
+                  disabled={isTestRunning}
+                />
+              </Grid>
+              {/* Propeller Model Input */}
+              <Grid item xs={12}>
+                <TextField
+                  label="Propeller Model"
+                  variant="outlined"
+                  value={propellerModel}
+                  onChange={(e) => setPropellerModel(e.target.value)}
+                  fullWidth
+                  size="small"
+                  margin="none"
+                  disabled={isTestRunning}
+                />
+              </Grid>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                mt: 4.75,
+                ml: 5,
+              }}
+            >
+              {/* Speed Input and Presets */}
+              <TextField
+                label="Speed"
                 variant="outlined"
+                value={speed}
+                type="number"
+                onChange={(e) => setSpeed(Number(e.target.value))}
+                fullWidth
+                size="small"
+                margin="none"
                 disabled={isTestRunning}
-                onClick={() => setSpeed((prev) => prev + btn.value)}
-                size="large"
-                style={{ width: '15px', minWidth: 'auto' }}
-
+                style={{ width: "18ex", minWidth: "auto" }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
               >
-                {btn.label}
-              </Button>
-            ))}
-          </Box>
-          </Box>
+                {speedPresets.map((btn, index) => (
+                  <Button
+                    key={index}
+                    variant="outlined"
+                    disabled={isTestRunning}
+                    onClick={() => setSpeed((prev) => prev + btn.value)}
+                    size="large"
+                    style={{ width: "15px", minWidth: "auto" }}
+                  >
+                    {btn.label}
+                  </Button>
+                ))}
+              </Box>
+            </Box>
 
-          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", mt: 4.75, ml: 1 }}>
-          {/* Duration Input and Presets */}       
-            <TextField
-              label="Duration(seconds)"
-              variant="outlined"
-              value={duration}
-              type="number"
-              onChange={(e) => setDuration(Number(e.target.value))}
-              fullWidth
-              size="small"
-              margin="none"
-              disabled={isTestRunning}
-              style={{ width: '18ex', minWidth: 'auto' }}
-              
-            />
-          
-          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-            {timePresets.map((btn, index) => (
-              <Button
-                key={index}
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                mt: 4.75,
+                ml: 1,
+              }}
+            >
+              {/* Duration Input and Presets */}
+              <TextField
+                label="Duration(seconds)"
                 variant="outlined"
+                value={duration}
+                type="number"
+                onChange={(e) => setDuration(Number(e.target.value))}
+                fullWidth
+                size="small"
+                margin="none"
                 disabled={isTestRunning}
-                onClick={() => setDuration((prev) => prev + btn.value)}
-                size="large"
-                style={{ width: '15px', minWidth: 'auto' }}
+                style={{ width: "18ex", minWidth: "auto" }}
+              />
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
               >
-                {btn.label}
-              </Button>
-            ))}
-          </Box>
-          </Box>
-        </Grid>
+                {timePresets.map((btn, index) => (
+                  <Button
+                    key={index}
+                    variant="outlined"
+                    disabled={isTestRunning}
+                    onClick={() => setDuration((prev) => prev + btn.value)}
+                    size="large"
+                    style={{ width: "15px", minWidth: "auto" }}
+                  >
+                    {btn.label}
+                  </Button>
+                ))}
+              </Box>
+            </Box>
+          </Grid>
         </div>
-      
-    </Box>
+      </Box>
     </div>
   );
 };
